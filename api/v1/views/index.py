c738ebc2e09a78944348de643355ff4489dd1aeb
olart <akanbi.ges201@gmail.com>
2022-04-16 21:21:55 +0100

    Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,24 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
 def return_response():
     """return the response status in form of json structure"""
     return jsonify(status="OK")
+
+
+@app_views.route('/stats', strict_slashes=False)
+def ret_number():
+    """JSON structure response"""
+    count = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(count)
