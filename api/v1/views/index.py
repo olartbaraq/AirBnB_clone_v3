c6e1a4d5b092876ffd57feaa1f6fba4c5f503dd1
olart <akanbi.ges201@gmail.com>
2022-04-18 09:09:17 +0100

    count

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,12 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.user import User
+from models.review import Review
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -16,11 +22,11 @@ def return_response():
 def ret_number():
     """JSON structure response"""
     count = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
     }
     return jsonify(count)
