1374e15fbbb3bd42ebc2b23b9cb8affb0119da0e
olart <akanbi.ges201@gmail.com>
2022-04-18 15:32:47 +0100

    adjust dic

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -21,12 +21,11 @@ def return_response():
 @app_views.route('/stats', strict_slashes=False)
 def ret_number():
     """JSON structure response"""
-    count = {
-        "amenities": storage.count(Amenity),
-        "cities": storage.count(City),
-        "places": storage.count(Place),
-        "reviews": storage.count(Review),
-        "states": storage.count(State),
-        "users": storage.count(User)
-    }
+    count = {}
+    count["amenities"] = storage.count('Amenity')
+    count["cities"] = storage.count('City')
+    count["places"] = storage.count('Place')
+    count["reviews"] = storage.count('Review')
+    count["states"] = storage.count('State')
+    count["users"] = storage.count('User')
     return jsonify(count)
