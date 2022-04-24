102cc698cfc4f4dd4e6ef02d8629b180c334343c
olartbaraq <88320030+olartbaraq@users.noreply.github.com>
2022-04-24 17:28:19 +0100

    update

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -12,22 +12,24 @@ from flask import jsonify, abort, request
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'])
 def places_list(city_id):
-    """ list of an objetc in a dict form """
+    """ list of an objetc in a dict form
+    """
     lista = []
-    dic = storage.all(City)
+    dic = storage.all('City')
     for elem in dic:
         if dic[elem].id == city_id:
             var = dic[elem].places
             for i in var:
                 lista.append(i.to_dict())
-                return (jsonify(lista))
+            return (jsonify(lista))
     abort(404)
 
 
 @app_views.route('/places/<place_id>', methods=['GET'])
 def place(place_id):
-    """ list of objetc in dict form """
-    dic = storage.all(Place)
+    """ list of objetc in dict form
+    """
+    dic = storage.all('Place')
     for elem in dic:
         if dic[elem].id == place_id:
             return (jsonify(dic[elem].to_dict()))
@@ -36,8 +38,9 @@ def place(place_id):
 
 @app_views.route('/places/<place_id>', methods=['DELETE'])
 def place_delete(place_id):
-    """ delete the delete """
-    dic = storage.all(Place)
+    """ delete the delete
+    """
+    dic = storage.all('Place')
     for key in dic:
         if place_id == dic[key].id:
             dic[key].delete()
@@ -48,7 +51,8 @@ def place_delete(place_id):
 
 @app_views.route('/cities/<city_id>/places', methods=['POST'])
 def add_place(city_id):
-    """ create a place of a specified city """
+    """ create a place of a specified city
+    """
     lista = []
     obj = storage.get("City", city_id)
     content = request.get_json()
@@ -73,7 +77,8 @@ def add_place(city_id):
 
 @app_views.route('/places/<place_id>', methods=['PUT'])
 def update_place(place_id):
-    """ update specified place """
+    """ update specified place
+    """
     dic = storage.all('Place')
     for key in dic:
         if place_id == dic[key].id:
@@ -89,3 +94,68 @@ def update_place(place_id):
                 dic[key].save()
                 return jsonify(dic[key].to_dict())
     abort(404)
+
+# ----------------------advanced task ---------------------------------------
+
+
+@app_views.route('/places_search', methods=['POST'])
+def advanced():
+    """ return all places per city, state or amenities
+    return all places that has all amenities
+    return all places that belong to city or state
+    permited keys states, cities, amenities
+    """
+    # rule 0
+    content = request.get_json(force=True, silent=True)
+    if content is None:
+        return jsonify('Not a JSON'), 400
+    # rule 1
+    result, places = [], []
+    if len(content) == 0:
+        places = storage.all("Place").values()
+        for elem in places:
+            result.append(elem.to_dict())
+        return jsonify(result)
+
+    flag = 0
+    for key in content:
+        if len(content[key]) > 0:
+            flag = 1
+            break
+    if flag == 0:
+        places = storage.all("Place").values()
+        for elem in places:
+            result.append(elem.to_dict())
+        return jsonify(result)
+    # rule 2
+    if "states" in content.keys() and len(content["states"]) > 0:
+        states = content["states"]
+        for id in states:
+            st = storage.get("State", id)
+            if st:
+                for city in st.cities:
+                    for pl in city.places:
+                        places.append(pl)
+    # rule 3
+    if "cities" in content.keys() and len(content["cities"]) > 0:
+        cities = content["cities"]
+        for id in cities:
+            ct = storage.get("City", id)
+            if ct:
+                for pl in ct.places:
+                    places.append(pl)
+
+    places = list(set(places))
+
+    if "amenities" in content.keys() and len(content["amenities"]) > 0:
+        ame = []
+        for id in content["amenities"]:
+            ame.append(storage.get("Amenity", id))
+        places = [pl for pl in places if all([a in pl.amenities for a in ame])]
+
+    for elem in places:
+        var = elem.to_dict()
+        if "amenities" in var.keys():
+            del var["amenities"]
+        result.append(var)
+    return jsonify(result)
